d61d3c2479c088213aaaa5858d1a640906d28d3e
Brent Pulmans <brent.pulmans@thomasmore.be>
2025-03-09 14:17:46 +0100

    Updates for academic year 2024-2025

--- queries/festival_queries.py
+++ queries/festival_queries.py
@@ -4,6 +4,6 @@ festival_name_and_province_query = "SELECT name, province FROM festivaldb.festiv
 
 festivals_by_province_query = "SELECT name, province FROM festivaldb.festival WHERE province = %s;"
 
-festivals_by_name_and_month_query = "SELECT name, location, startDate, endDate, province FROM festivaldb.festival WHERE name LIKE %s AND (date_format(startDate, '%m') = %s OR date_format(endDate, '%m') = %s);"
+festivals_by_name_and_month_query = "SELECT name, location, startDate, endDate, province FROM festivaldb.festival WHERE name LIKE %s AND (TO_CHAR(startDate, 'FMMM') = %s OR TO_CHAR(endDate, 'FMMM') = %s);"
 
 insert_festival_query = "INSERT INTO festivaldb.festival (name, location, startDate, endDate, province, comment) VALUES (%s, %s, %s, %s, %s, %s);"
